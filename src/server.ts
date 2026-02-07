dc96da44cc9a33819fc23cd18c6a37a256b7dfcc
Crocrodile <cyril.lamand03@gmail.com>
2026-02-07 21:06:24 +0100

    feat(api): 6.1

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,15 @@ import express from 'express';
 const app = express()
 const port = 3000;
 
+const etudiants = [
+  {id: 1, nom: "Dupont", prenom: "Jean"},
+  {id: 2, nom: "Martin", prenom: "Sophie"},
+  {id: 3, nom: "Doe", prenom: "John"}
+];
+
+
 app.get('/', (req: Request, res: Response): void  => {
-  res.send('Bienvenu sur mon server api !');
+  res.json(etudiants);
 });
 
 app.listen(port, () => {
