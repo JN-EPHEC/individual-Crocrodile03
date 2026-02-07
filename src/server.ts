adb870f89e11834401af344aa54d179b975c7498
Crocrodile <cyril.lamand03@gmail.com>
2026-02-07 21:25:16 +0100

    feat(api): 6.2

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,15 @@ app.get('/', (req: Request, res: Response): void  => {
   res.json(etudiants);
 });
 
+app.get('/api/hello/:name', (req: Request, res: Response) => {
+  const name: String = req.params.name;
+  const date = new Date();
+  res.json({ message: `Bonjour ${name}`, timestamp: date.toISOString() });
+})
+
+
+
+
 app.listen(port, () => {
   console.log('http://localhost:' + port);
 })
